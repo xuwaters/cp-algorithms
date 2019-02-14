960ba46c3c035a6e5af4b8ecdcfffbeaf3c0d2c9
Will <xuwaters@gmail.com>
2019-02-13 18:16:53 -0800

    euler path

---
+++ 09-graphs/06-cycles/03-eulerian-path/euler_path.go
@@ -0,0 +1,12 @@
+package euler_path
+
+// 
+// 1. all vertices are even degree
+//   start from any vertex
+// 
+// 2. two vertices are odd degree
+//   start from a vertex of odd degree
+// 
+// dfs
+// 
+
